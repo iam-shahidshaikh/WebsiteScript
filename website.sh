25fbbaa1d896f586c5f87649fb5f588cbc47bd0d
SHAHID SHAIKH <102761235+iam-shahidshaikh@users.noreply.github.com>
2025-03-07 01:09:44 +0530

    Add files via upload

---
+++ website.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install wget unzip httpd -y
+sudo systemctl start httpd
+systemctl enable httpd
+sudo wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+sudo unzip -o 2117_infinite_loop.zip
+sudo cp -r 2117_infinite_loop/* /var/www/html/
+sudo systemctl restart httpd
